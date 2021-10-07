9739b8e9fcee6875fe87a72f91d42104775913fa
Xavier Hernandez <xhernan.martinez@gmail.com>
2021-10-07 20:39:37 +0200

    initial styling + post page styling

--- pages/posts/_id/index.vue
+++ pages/posts/_id/index.vue
@@ -20,6 +20,57 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+  .single-post-page {
+    padding: 30px;
+    text-align: center;
+    box-sizing: border-box;
+  }
 
+  .post {
+    width: 100%;
+  }
+
+  @media (min-width: 768px) {
+    .post {
+      width: 600px;
+      margin: auto;
+    }
+  }
+
+  .post-title {
+    margin: 0px;
+    text-transform: uppercase;
+  }
+
+  .post-details {
+    padding: 10px;
+    box-sizing: border-box;
+    border-bottom: 3px solid #ccc;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
+
+  @media (min-width: 768px) {
+    .post-details {
+      flex-direction: row;
+    }
+  }
+  
+  .post-detail {
+    color: rgb(88, 88, 88);
+    margin: 0 10px;
+  }
+
+  .post-feedback a {
+    text-decoration: none;
+    color: red;
+  }
+
+  .post-feedback a:hover,
+  .post-feedback a:active {
+    color: salmon;
+  }
 </style>
