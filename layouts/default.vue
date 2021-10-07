92c030feea7f7a9bedeccb18e5cdf908f1a14ced
Xavier Hernandez <xhernan.martinez@gmail.com>
2021-10-07 21:01:20 +0200

    landing page init design

--- layouts/default.vue
+++ layouts/default.vue
@@ -11,10 +11,6 @@ export default {
 <style>
 html {
   font-family: 'Open Sans', sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
   -moz-osx-font-smoothing: grayscale;
   -webkit-font-smoothing: antialiased;
   box-sizing: border-box;
